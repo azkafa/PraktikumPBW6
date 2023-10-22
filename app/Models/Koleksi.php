8efbebd3b578eb26dbab60a04054df59296a376e
azkafa <ssekaa1305@gmail.com>
2023-10-22 12:13:12 +0700

    Done

---
+++ app/Models/Koleksi.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Koleksi extends Model
+{
+    use HasFactory;
+    // Nama    : Azka Faris akbar
+    // NIM     : 6706220020
+    // Kelas   : D3IF-4603
+    protected $table = 'koleksi';
+    
+    protected $fillable = [
+        'namaKoleksi',
+        'jenisKoleksi',
+        'jumlahKoleksi',
+        'jumlahKeluar',
+        'jumlahSisa'
+    ];
+}
---
+++ resources/views/koleksi/viewKoleksi.blade.php
@@ -0,0 +1,8 @@
+ <!-- 
+Nama    : Azka Faris Akbar
+NIM     : 6706220020
+Kelas   : D3IF-4603 
+-->
+<div class="btn-group">
+    <a href="{{ route('koleksi.infoKoleksi', $id) }}" class="btn btn-sm btn-dark">Info</a>
+</div>
